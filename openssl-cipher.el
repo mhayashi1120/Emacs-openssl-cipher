6ef71247839a837598d103cf1a7006678fdde978
Hayashi Masahiro <mhayashi1120@gmail.com>
2011-05-12 20:29:27 +0900

    divide test code

--- openssl-cipher.el
+++ openssl-cipher.el
@@ -2,7 +2,7 @@
 
 ;; Author: Masahiro Hayashi <mhayashi1120@gmail.com>
 ;; Keywords: openssl encrypt decrypt password
-;; URL: todo
+;; URL: http://github.com/mhayashi1120/Emacs-openssl-cipher/raw/master/openssl-cipher.el
 ;; Emacs: GNU Emacs 22 or later
 ;; Version 0.5.0
 
@@ -186,48 +186,6 @@
   (let ((vec (make-vector 1 nil)))
     (set (intern algorithm vec) string)
     vec))
-
-(dont-compile
-  (when (and (fboundp 'with-mock)
-             (fboundp 'expectations))
-    (expectations 
-      ;; ascii as unibyte array
-      (expect "abcd"
-        (with-mock
-          (stub read-passwd => (copy-sequence "pass"))
-          (let ((enc (openssl-cipher-encrypt-unibytes "abcd")))
-            (openssl-cipher-decrypt-unibytes enc))))
-
-      ;; ascii as unibyte string
-      (expect "abcd"
-        (with-mock
-          (stub read-passwd => (copy-sequence "pass"))
-          (let ((enc (openssl-cipher-encrypt-string "abcd")))
-            (openssl-cipher-decrypt-string enc))))
-
-      ;; check binary string
-      (expect "\316\323"
-        (with-mock
-          (stub read-passwd => (copy-sequence "pass"))
-          (let ((enc (openssl-cipher-encrypt-unibytes "\316\323")))
-            (openssl-cipher-decrypt-unibytes enc))))
-
-      (expect "test マルチバイト文字"
-        (with-mock
-          (stub read-passwd => (copy-sequence "pass"))
-          (let ((enc (openssl-cipher-encrypt-string "test マルチバイト文字")))
-            (openssl-cipher-decrypt-string enc))))
-
-      ;; invalid password.
-      (expect (error)
-        (with-mock
-          (stub read-passwd => (lexical-let ((passes `("pass" "invaild"))) (pop passes)))
-          (let ((enc (openssl-cipher-encrypt-string "test string")))
-            (openssl-cipher-decrypt-string enc))))
-      )))
-
-;; (progn (require 'el-mock) (require 'el-expectations))
-;; (expectations-execute)
 
 (provide 'openssl-cipher)
 
