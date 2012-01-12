9692f243c1e0d7169005b75bff6f6252fb2ad6ab
Masahiro Hayashi <mhayashi1120@gmail.com>
2012-01-12 17:13:31 +0900

    use `ert' package

--- openssl-cipher-test.el
+++ openssl-cipher-test.el
@@ -1,40 +1,31 @@
-(require 'el-mock)
-(require 'el-expectations)
+(require 'ert)
 
-(expectations 
-  ;; ascii as unibyte array
-  (expect "abcd"
-    (with-mock
-      (stub read-passwd => (copy-sequence "pass"))
-      (let ((enc (openssl-cipher-encrypt-unibytes "abcd")))
-        (openssl-cipher-decrypt-unibytes enc))))
+(ert-deftest openssl-cipher-normal ()
+  "Normal test (for ascii)"
+  :tags '(openssl-cipher)
+  (flet ((read-passwd (&rest dummy) (copy-sequence "pass")))
+    ;; ascii as unibyte array
+    (let ((enc (openssl-cipher-encrypt-unibytes "abcd")))
+      (should (equal (openssl-cipher-decrypt-unibytes enc) "abcd")))
+    ;; ascii as unibyte string
+    (let ((enc (openssl-cipher-encrypt-string "abcd")))
+      (should (equal (openssl-cipher-decrypt-string enc) "abcd")))))
 
-  ;; ascii as unibyte string
-  (expect "abcd"
-    (with-mock
-      (stub read-passwd => (copy-sequence "pass"))
-      (let ((enc (openssl-cipher-encrypt-string "abcd")))
-        (openssl-cipher-decrypt-string enc))))
+(ert-deftest openssl-cipher-normal-multibyte ()
+  "Normal test (for multibyte string)"
+  :tags '(openssl-cipher)
+  (flet ((read-passwd (&rest dummy) (copy-sequence "pass")))
+    ;; check binary string
+    (let ((enc (openssl-cipher-encrypt-unibytes "\316\323")))
+      (should (equal (openssl-cipher-decrypt-unibytes enc) "\316\323")))
+    (let ((enc (openssl-cipher-encrypt-string "test マルチバイト文字")))
+      (should (equal (openssl-cipher-decrypt-string enc) "test マルチバイト文字")))))
 
-  ;; check binary string
-  (expect "\316\323"
-    (with-mock
-      (stub read-passwd => (copy-sequence "pass"))
-      (let ((enc (openssl-cipher-encrypt-unibytes "\316\323")))
-        (openssl-cipher-decrypt-unibytes enc))))
-
-  (expect "test マルチバイト文字"
-    (with-mock
-      (stub read-passwd => (copy-sequence "pass"))
-      (let ((enc (openssl-cipher-encrypt-string "test マルチバイト文字")))
-        (openssl-cipher-decrypt-string enc))))
-
-  ;; invalid password.
-  (expect (error)
-    (with-mock
-      (stub read-passwd => (lexical-let ((passes `("pass" "invaild"))) (pop passes)))
+(ert-deftest openssl-cipher-invalid-password ()
+  "Invalid password."
+  :tags '(openssl-cipher)
+  (lexical-let ((passes `("pass" "invaild")))
+    (flet ((read-passwd (&rest dummy) (pop passes)))
       (let ((enc (openssl-cipher-encrypt-string "test string")))
-        (openssl-cipher-decrypt-string enc))))
-  )
+        (should-error (openssl-cipher-decrypt-string enc))))))
 
-(expectations-execute)
