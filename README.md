ec3755b88f7b37d4a7e833c1fe78a611db99b8e5
Masahiro Hayashi <mhayashi1120@gmail.com>
2014-12-05 20:38:30 +0900

    add/fix doc

---
+++ README.md
@@ -0,0 +1,47 @@
+openssl-cipher.el
+=================
+
+* Install:
+
+1. Install `openssl' command to your system by
+  apt, yum... whatever you desire.
+
+  Source code: http://www.openssl.org/source/
+
+2. Put this file into load-path'ed directory, and byte compile it
+  if desired. And put the following expression into your ~/.emacs.
+
+     (require 'openssl-cipher)
+
+* Usage:
+
+** To encrypt a well encoded string (High level API)
+
+ `openssl-cipher-encrypt-string' <-> `openssl-cipher-decrypt-string'
+
+** To encrypt a binary string (Middle level API)
+
+ `openssl-cipher-encrypt-unibytes' <-> `openssl-cipher-decrypt-unibytes'
+
+** To encrypt a binary string (Low level API)
+
+ `openssl-cipher-encrypt' <-> `openssl-cipher-decrypt'
+
+** To encrypt a file
+
+ `openssl-cipher-encrypt-file' <-> `openssl-cipher-decrypt-file'
+
+* Sample:
+
+** To encrypt my secret
+  Please ensure that do not forget `clear-string' you want to hide.
+
+    (defvar my-secret nil)
+
+    (let ((raw-string "My Secret"))
+      (setq my-secret (openssl-cipher-encrypt-string raw-string))
+      (clear-string raw-string))
+
+** To decrypt `my-secret'
+
+    (openssl-cipher-decrypt-string my-secret)
--- openssl-cipher.el
+++ openssl-cipher.el
@@ -4,7 +4,7 @@
 ;; Keywords: data, convenience, files
 ;; URL: https://github.com/mhayashi1120/Emacs-openssl-cipher/raw/master/openssl-cipher.el
 ;; Emacs: GNU Emacs 22 or later
-;; Version 0.7.2
+;; Version 0.7.3
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -23,42 +23,51 @@
 
 ;;; Install:
 
-;; Put this file into load-path'ed directory, and byte compile it if
-;; desired. And put the following expression into your ~/.emacs.
+;; 1. Install `openssl' command to your system by
+;;  apt, yum... whatever you desire.
+;;
+;;  Source code: http://www.openssl.org/source/
+
+;; 2. Put this file into load-path'ed directory, and byte compile it
+;;  if desired. And put the following expression into your ~/.emacs.
 ;;
 ;;     (require 'openssl-cipher)
 
 ;;; Usage:
 
-;; * To encrypt a well encoded string (High level API)
-;; `openssl-cipher-encrypt-string' <-> `openssl-cipher-decrypt-string'
+;; ** To encrypt a well encoded string (High level API)
 ;;
-;; * To encrypt a binary string (Middle level API)
-;; `openssl-cipher-encrypt-unibytes' <-> `openssl-cipher-decrypt-unibytes'
+;;  `openssl-cipher-encrypt-string' <-> `openssl-cipher-decrypt-string'
 
-;; * To encrypt a binary string (Low level API)
-;; `openssl-cipher-encrypt' <-> `openssl-cipher-decrypt'
+;; ** To encrypt a binary string (Middle level API)
+;;
+;;  `openssl-cipher-encrypt-unibytes' <-> `openssl-cipher-decrypt-unibytes'
 
-;; * To encrypt a file
-;; `openssl-cipher-encrypt-file' <-> `openssl-cipher-decrypt-file'
+;; ** To encrypt a binary string (Low level API)
+;;
+;;  `openssl-cipher-encrypt' <-> `openssl-cipher-decrypt'
+
+;; ** To encrypt a file
+;;
+;;  `openssl-cipher-encrypt-file' <-> `openssl-cipher-decrypt-file'
 
 ;;; Sample:
 
-;; * To encrypt my secret
-;;   Please ensure that do not forget `clear-string' you want to hide.
+;; ** To encrypt my secret
+;;  Please ensure that do not forget `clear-string' you want to hide.
+;;
+;;     (defvar my-secret nil)
+;;
+;;     (let ((raw-string "My Secret"))
+;;       (setq my-secret (openssl-cipher-encrypt-string raw-string))
+;;       (clear-string raw-string))
 
-;; (defvar my-secret nil)
-
-;; (let ((raw-string "My Secret"))
-;;   (setq my-secret (openssl-cipher-encrypt-string raw-string))
-;;   (clear-string raw-string))
-
-;; * To decrypt `my-secret'
-
-;; (openssl-cipher-decrypt-string my-secret)
+;; ** To decrypt `my-secret'
+;;
+;;     (openssl-cipher-decrypt-string my-secret)
 
 ;;; TODO:
-;; * should not use temporary file?
+;; * should not use temporary file? (man shred)
 
 ;;; Code:
 
