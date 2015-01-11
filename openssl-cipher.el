99c834b1392fb530aa32b698ec6168fb62646c29
Masahiro Hayashi <mhayashi1120@gmail.com>
2015-01-11 09:52:36 +0900

    fix doc for markdown

--- openssl-cipher.el
+++ openssl-cipher.el
@@ -4,7 +4,7 @@
 ;; Keywords: data, convenience, files
 ;; URL: https://github.com/mhayashi1120/Emacs-openssl-cipher/raw/master/openssl-cipher.el
 ;; Emacs: GNU Emacs 22 or later
-;; Version 0.7.3
+;; Version 0.7.4
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -21,50 +21,55 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
+;;; Commentary:
+
+;; `openssl-cipher` is a library to encrypt/decrypt string or file with
+;; `openssl` command.
+
 ;; ## Install:
 
-;; 1. Install `openssl' command to your system by
+;; 1. Install `openssl` command to your system by
 ;;  apt, yum... whatever you desire.
-;;
+
 ;;  Source code: http://www.openssl.org/source/
 
 ;; 2. Put this file into load-path'ed directory, and byte compile it
 ;;  if desired. And put the following expression into your ~/.emacs.
-;;
-;;     (require 'openssl-cipher)
+
+;;       (require 'openssl-cipher)
 
 ;; ## Usage:
 
-;; ** To encrypt a well encoded string (High level API)
-;;
-;;  `openssl-cipher-encrypt-string' <-> `openssl-cipher-decrypt-string'
+;; * To encrypt a well encoded string (High level API)
 
-;; ** To encrypt a binary string (Middle level API)
-;;
-;;  `openssl-cipher-encrypt-unibytes' <-> `openssl-cipher-decrypt-unibytes'
+;;  `openssl-cipher-encrypt-string` <-> `openssl-cipher-decrypt-string`
 
-;; ** To encrypt a binary string (Low level API)
-;;
-;;  `openssl-cipher-encrypt' <-> `openssl-cipher-decrypt'
+;; * To encrypt a binary string (Middle level API)
 
-;; ** To encrypt a file
-;;
-;;  `openssl-cipher-encrypt-file' <-> `openssl-cipher-decrypt-file'
+;;  `openssl-cipher-encrypt-unibytes` <-> `openssl-cipher-decrypt-unibytes`
+
+;; * To encrypt a binary string (Low level API)
+
+;;  `openssl-cipher-encrypt` <-> `openssl-cipher-decrypt`
+
+;; * To encrypt a file
+
+;;  `openssl-cipher-encrypt-file` <-> `openssl-cipher-decrypt-file`
 
 ;; ## Sample:
 
-;; ** To encrypt my secret
-;;  Please ensure that do not forget `clear-string' you want to hide.
-;;
-;;     (defvar my-secret nil)
-;;
-;;     (let ((raw-string "My Secret"))
-;;       (setq my-secret (openssl-cipher-encrypt-string raw-string))
-;;       (clear-string raw-string))
+;; * To encrypt my secret
+;;  Please ensure that do not forget `clear-string` you want to hide.
 
-;; ** To decrypt `my-secret'
-;;
-;;     (openssl-cipher-decrypt-string my-secret)
+;;       (defvar my-secret nil)
+
+;;       (let ((raw-string "My Secret"))
+;;         (setq my-secret (openssl-cipher-encrypt-string raw-string))
+;;         (clear-string raw-string))
+
+;; * To decrypt `my-secret`
+
+;;         (openssl-cipher-decrypt-string my-secret)
 
 ;;; TODO:
 ;; * should not use temporary file? (man shred)
