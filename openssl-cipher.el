cea27ff0428d8bbb8db9e21a9510cf20a54a8aa9
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-02-16 17:10:25 +0900

    add cl-lib requires (reported by package-lint)

--- openssl-cipher.el
+++ openssl-cipher.el
@@ -5,7 +5,7 @@
 ;; URL: https://github.com/mhayashi1120/Emacs-openssl-cipher
 ;; Emacs: GNU Emacs 24 or later
 ;; Version: 0.8.1
-;; Package-Requires: ((emacs "24"))
+;; Package-Requires: ((emacs "24") (cl-lib "0.7.1"))
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
